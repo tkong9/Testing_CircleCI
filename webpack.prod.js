bd67019ee846b6bf1de675c72a749f591383d003
tkong9 <taewon2711@gmail.com>
2020-07-10 17:23:59 +0900

    modify yml to generate jest result

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,3 @@
-// const path = require('path');
-// const { merge } = require('webpack-merge');
-// const common = require('./webpack.common');
-
-// module.exports = merge(common, {
-//   mode: 'production',
-//   output: {
-//     filename: 'main.[contentHash].js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-// });
-
 const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebpackPlugin = require('html-webpack-plugin');
